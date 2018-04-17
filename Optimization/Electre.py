12f7c753e27127038b5c088f1f040f497ef38ce2
Alejandro Salgado G <asalgad2@eafit.edu.co>
2018-04-17 14:17:25 -0500

    Finishing electre method, optimization

--- Optimization/Electre.py
+++ Optimization/Electre.py
@@ -1,9 +1,11 @@
-#!/bin/env python
+#!/usr/bin/env python3
 
 from Electre_functions import *
 
 def electre(A, w, min_max):
     print_matrix("A", A)
+    print_vector("w", w)
+    print_vector("Intention (0 minimize, 1 maximize)", min_max)
 
     concordance = get_concordance(A, w, min_max)
     print_matrix("Corcordance matrix", concordance)
@@ -21,6 +23,16 @@ def electre(A, w, min_max):
     d = get_mean(discordance)
     print("Umbrals c=%.2f, d=%.2f" % (c,d))
 
+    dom_concordant = get_dom_concordant(c, concordance)
+    print_matrix("Concordant dominance", dom_concordant)
+
+    dom_discordant = get_dom_discordant(d, discordance)
+    print_matrix("Discordant dominance", dom_discordant)
+
+    dominance = get_dominance(dom_concordant, dom_discordant)
+    print_matrix("Dominance matrix", dominance)
+
+
 def main():
     A = [
           [  1,  8,  1  ],
@@ -31,7 +43,7 @@ def main():
         ]  
 
     w = [ 0.588, 0.294, 0.118 ]
-    min_max = [ "min", "min", "min" ]
+    min_max = [ minimize, minimize, minimize ]
 
     electre(A,w, min_max)
 
--- Optimization/Electre_functions.py
+++ Optimization/Electre_functions.py
@@ -1,5 +1,8 @@
 import sys
 
+minimize = 0 
+maximize = 1 
+
 def get_0_matrix(height, width):
     return [ [ 0 for j in range(width) ] for i in range(height) ]
 
@@ -12,6 +15,14 @@ def print_matrix(title, A):
         for j in range(width):
             sys.stdout.write( " %.2f " % A[i][j])
         print( "" )
+
+def print_vector(title, b):
+    length = len(b)
+
+    print(title)
+    for i in range(length):
+        sys.stdout.write( " %.2f " % b[i])
+    print( "" )
 
 def get_transpose(A):
     alt_num = len(A)
@@ -32,10 +43,10 @@ def calculate_concordance(i,j, A, w, min_max):
         if A[i][k] == A[j][k]:
             concordance += w[k]/2
 
-        if min_max[k] == "max" and A[i][k] > A[j][k]:
+        if min_max[k] == maximize and A[i][k] > A[j][k]:
             concordance += w[k]
 
-        if min_max[k] == "min" and A[i][k] < A[j][k]:
+        if min_max[k] == minimize and A[i][k] < A[j][k]:
                 concordance += w[k]
 
     return concordance
@@ -95,8 +106,8 @@ def calculate_discordance(i,j, N, min_max):
         val_i = N[i][k]
         val_j = N[j][k]
 
-        check_1 = min_max[k] == "max" and val_i < val_j
-        check_2 = min_max[k] == "min" and val_i > val_j
+        check_1 = min_max[k] == maximize and val_i < val_j
+        check_2 = min_max[k] == minimize and val_i > val_j
 
         if check_1 or check_2:
             diff_num = abs(val_j - val_i)
@@ -121,4 +132,50 @@ def get_discordance(N, min_max):
     return discordance
 
 def get_mean(A):
-    return 0
+    alt_num = len(A)
+    criteria_num = len(A[0])
+
+    total_sum = 0
+    total_elem = alt_num * criteria_num
+
+    for i in range(alt_num):
+        for j in range(criteria_num):
+            total_sum += A[i][j]
+
+    return total_sum/total_elem
+
+def get_dom_concordant(c, A):
+    alt_num = len(A)
+    criteria_num = len(A[0])
+
+    dom_concordant = get_0_matrix(alt_num, criteria_num)
+
+    for i in range(alt_num):
+        for j in range(criteria_num):
+            if A[i][j] > c and i != j:
+                dom_concordant[i][j] = 1
+    return dom_concordant
+
+def get_dom_discordant(d, A):
+    alt_num = len(A)
+    criteria_num = len(A[0])
+
+    dom_discordant = get_0_matrix(alt_num, criteria_num)
+
+    for i in range(alt_num):
+        for j in range(criteria_num):
+            if A[i][j] < d and i != j:
+                dom_discordant[i][j] = 1
+    return dom_discordant
+
+def get_dominance(concordance, discordance):
+    alt_num = len(concordance)
+    criteria_num = len(concordance[0])
+
+    dominance = get_0_matrix(alt_num, criteria_num)
+
+    for i in range(alt_num):
+        for j in range(criteria_num):
+            dominance[i][j] = concordance[i][j] * discordance[i][j]
+
+    return dominance
