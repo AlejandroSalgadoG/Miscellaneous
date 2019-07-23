812544b8a380838ac687ba5c7ff9e4a242e8c781
Alejandro Salgado G <asalgad2@eafit.edu.co>
2019-07-23 16:33:25 -0500

    Finishing brownian movement

--- Stocastic/Brownian.py
+++ Stocastic/Brownian.py
@@ -2,21 +2,24 @@ import numpy as np
 from math import sqrt
 import matplotlib.pyplot as plt
 
-n = 100
-k = 1000
-
 def get_norm_numbers(mu, sigma, shape):
     return np.random.normal(mu, sigma, shape)
 
-et = get_norm_numbers(mu=0, sigma=1, shape=[k, n])
-bt = np.zeros(shape=[k,n])
-time = np.arange(n)
+def brownian_movement(n, k, dt):
+    et = get_norm_numbers(mu=0, sigma=1, shape=[k, n-1])
+    bt = np.zeros(shape=[k,n])
+    t = np.arange(0,n*dt,dt)
+    
+    sqrt_dt = sqrt(dt)
+    
+    for i in range(k):
+        for j in range(1,n):
+            bt[i][j] = bt[i][j-1] + sqrt_dt * et[i][j-1]
 
-sqrt_dt = sqrt(1/n)
+    return bt, t
 
-for i in range(k):
-    for t in time[1:]:
-        bt[i][t] = bt[i][t-1] + sqrt_dt * et[i][t-1]
-    plt.plot(time, bt[i], linewidth=1)
+simulations, time = brownian_movement(n=100, k=1000, dt=0.01)
 
+for simulation in simulations:
+    plt.plot(time, simulation, linewidth=1)
 plt.show()
