b85c58fbd9fcf2398936b0ee2acd238e96ff2fdb
Alejandro Salgado G <asalgad2@eafit.edu.co>
2019-10-18 10:36:01 -0500

    Adding estimation method for EDE

---
+++ Stochastic/Estimation.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from Brownian import standard_brownian
+from Numerical import homogeneous_equation
+
+def instant_returns(n, x_t):
+    r = np.zeros(n)
+    for i in range(1,n):
+        r[i] = (x_t[i] - x_t[i-1])/x_t[i-1]
+    return r
+
+if __name__ == '__main__':
+    x_0, u, s, n = 1, 1.5, 2.5, 500
+
+    [bt], [b1], time = standard_brownian(n, 1, 1/n)
+    x_t = homogeneous_equation(x_0, u, s, n, bt, time)
+
+    r_t = instant_returns(n, x_t)
+    dt = 1/n
+    u = np.mean(r_t) / dt
+    s = np.sqrt( np.var(r_t, ddof=0) / dt )
+    print(u, s)
