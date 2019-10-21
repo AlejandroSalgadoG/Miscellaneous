Fixing convergence calculation and estimation print in EDE methods

Stochastic/Estimation.py
@@ -13,11 +13,14 @@ def instant_returns(n, x_t):
 if __name__ == '__main__':
     x_0, u, s, n = 1, 1.5, 2.5, 500
 
-    [bt], [b1], time = standard_brownian(n, 1, 1/n)
-    x_t = homogeneous_equation(x_0, u, s, n, bt, time)
+    print("real: u = %f, s = %f" % (u, s))
 
-    r_t = instant_returns(n, x_t)
-    dt = 1/n
-    u = np.mean(r_t) / dt
-    s = np.sqrt( np.var(r_t, ddof=0) / dt )
-    print(u, s)
+    for i in range(10):
+        [bt], [b1], time = standard_brownian(n, 1, 1/n)
+        x_t = homogeneous_equation(x_0, u, s, n, bt, time)
+
+        r_t = instant_returns(n, x_t)
+        dt = 1/n
+        u = np.mean(r_t) / dt
+        s = np.sqrt( np.var(r_t, ddof=0) / dt )
+        print("u = %f, s = %f" % (u, s))

Stochastic/Numerical.py
@@ -43,9 +43,8 @@ if __name__ == '__main__':
     x_t_e = euler(x_0, u, s, n, b1)
     x_t_m = milstein(x_0, u, s, n, b1)
     
-    plt.plot(time, x_t, linewidth=1)
     plt.plot(time, x_t_m, linewidth=1)
-    plt.legend(["x_t", "milstein"])
+    #plt.legend(["x_t", "milstein"])
     plt.show()
 
     print("euler \t\t milstein \t dt")
@@ -55,7 +54,7 @@ if __name__ == '__main__':
         x_t_e = euler(x_0, u, s, n, b1)
         x_t_m = milstein(x_0, u, s, n, b1)
 
-        error_e = np.mean(x_t - x_t_e)
-        error_m = np.mean(x_t - x_t_m)
+        error_e = np.mean(np.abs(x_t - x_t_e))
+        error_m = np.mean(np.abs(x_t - x_t_m))
 
         print("%f \t %f \t %f" % (error_e, error_m, 1/n) )