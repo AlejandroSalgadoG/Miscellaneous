454d258f6f488c707c08d84cbb94710c17a08e38
Alejandro Salgado G <asalgad2@eafit.edu.co>
2019-11-08 15:40:36 -0500

    Adding first version of finite differences

--- Stochastic/OptionValuation.py
+++ Stochastic/OptionValuation.py
@@ -2,6 +2,8 @@ import numpy as np
 from scipy.stats import norm
 import matplotlib.pyplot as plt
 
+np.set_printoptions(linewidth=1000, precision=2)
+
 from Brownian import standard_brownian
 from Numerical import homogeneous_equation
 
@@ -39,15 +41,41 @@ def binomial_tree(x_0, r, s, n):
                 f[i,j] = np.exp(-r*dt) * (p*f[i+1,j+1] + (1-p)*f[i+1,j])
     return f[0,0]
 
+def finite_differences(x_0, r, s, m, T, k):
+    x_max = x_0 * 3
+    dx = x_max / m
+    n = int( T*s**2*m**2 ) + 1
+    dt = T/n
+
+    assert(dt <= (dx**2/ (s*x_max)**2 ))
+
+    a = lambda j: dt/(r*dt+1) * (0.5*s**2*j**2 - 0.5*r*j)
+    b = lambda j: dt/(r*dt+1) * (1/dt - s**2*j**2)
+    c = lambda j: dt/(r*dt+1) * (0.5*s**2*j**2 + 0.5*r*j)
+
+    f = np.zeros([n+1, m+1])
+    f[:, m] = np.maximum(x_max-k, 0)
+    f[n, :] = [np.maximum(j*dx-k, 0) for j in range(m+1)]
+
+    for i in range(n-1,-1,-1):
+        for j in range(m-1,0,-1):
+            f[i,j] = a(j)*f[i+1,j-1] + b(j)*f[i+1,j] + c(j)*f[i+1,j+1]
+        print("%d\r" % i, end='')
+
+
+    print(f)
 
 if __name__ == '__main__':
     x_0, u, s, n, T, m, w, k = 100, 0.05, 0.3, 500, 1, 100, 100, 100
 
-    fcall = black_scholes(x_0, u, s, k)
-    print(fcall)
+#    fcall = black_scholes(x_0, u, s, k)
+#    print(fcall)
+#
+#    fcall_hat, fcalls = montecarlo(x_0, u, s, n, T, m, w, k)
+#    print(fcall_hat)
+#
+#    fcall_hat = binomial_tree(x_0, u, s, n)
+#    print(fcall_hat)
 
-    fcall_hat, fcalls = montecarlo(x_0, u, s, n, T, m, w, k)
-    print(fcall_hat)
-
-    fcall_hat = binomial_tree(x_0, u, s, n)
+    fcall_hat = finite_differences(x_0, 0.05, 0.2, 20, 1, k)
     print(fcall_hat)
