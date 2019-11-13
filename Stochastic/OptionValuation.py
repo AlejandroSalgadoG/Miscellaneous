35b3f75b3c1ecd8bdc8b711050f88d1989cfec2b
Alejandro Salgado G <asalgad2@eafit.edu.co>
2019-11-13 16:31:44 -0500

    Updating parameters in OptionValuation

--- Stochastic/OptionValuation.py
+++ Stochastic/OptionValuation.py
@@ -21,12 +21,12 @@ def montecarlo(x_0, r, s, n, T, m, w, k):
 
     return np.mean(fcall), fcall
 
-def black_scholes(x_0, r, s, k):
-    d1 = ( np.log(x_0/k) + (r+s**2/2) ) / s
-    d2 = d1 - s
-    return x_0 * norm.cdf(d1) - k*np.exp(-r)*norm.cdf(d2)
+def black_scholes(x_0, r, s, k, T):
+    d1 = ( np.log(x_0/k) + (r+s**2/2)*T ) / (s*np.sqrt(T))
+    d2 = d1 - s*np.sqrt(T)
+    return x_0 * norm.cdf(d1) - k*np.exp(-r*T)*norm.cdf(d2)
 
-def binomial_tree(x_0, r, s, n):
+def binomial_tree(x_0, r, s, k, n):
     dt = 1/n
     u = np.exp(s*np.sqrt(dt))
     d = np.exp(-s*np.sqrt(dt))
@@ -48,41 +48,40 @@ def finite_differences(vol, int_rate, strike, expiration, nas):
     nts = int(expiration/dt) + 1
     dt = expiration / nts
 
-    S = np.zeros(nas+1) 
-    v = np.zeros([nas+1, nts+1])
+    S = np.zeros(nas+1)
+    f = np.zeros([nas+1, nts+1])
 
     for i in range(nas+1):
         S[i] = i*ds
-        v[i,0] = np.maximum(S[i]-strike, 0)
+        f[i,0] = np.maximum(S[i]-strike, 0)
 
     for k in range(1,nts+1):
         for i in range(1,nas):
-            delta = (v[i+1, k-1] - v[i-1,k-1]) / (2*ds)
-            gamma = (v[i+1, k-1] - 2*v[i,k-1] + v[i-1,k-1]) / ds**2
-            theta = -0.5 * vol**2 * S[i]**2 * gamma - int_rate * S[i] * delta + int_rate * v[i,k-1]
+            delta = (f[i+1, k-1] - f[i-1,k-1]) / (2*ds)
+            gamma = (f[i+1, k-1] - 2*f[i,k-1] + f[i-1,k-1]) / ds**2
+            theta = -0.5 * vol**2 * S[i]**2 * gamma - int_rate * S[i] * delta + int_rate * f[i,k-1]
 
-            v[i,k] = v[i,k-1] - dt * theta
+            f[i,k] = f[i,k-1] - dt * theta
 
-        v[0,k] = v[0,k-1] * (1 - int_rate * dt)
-        v[nas,k] = 2 * v[nas-1,k] - v[nas-2,k]
+        f[0,k] = f[0,k-1] * (1 - int_rate * dt)
+        f[nas,k] = 2 * f[nas-1,k] - f[nas-2,k]
         print("%.2f%%" % (k/nts*100), end='\r')
 
-    ans_pos = np.argwhere(v[:,0]>0)[0]
-    return v[ans_pos,-1]
+    ans_pos = np.argwhere(f[:,0]>0)[0]
+    return f[ans_pos-1,-1]
 
 if __name__ == '__main__':
-    x_0, u, s, n, T, m, w, k = 100, 0.05, 0.3, 500, 1, 100, 100, 100
+    x_0, u, s, n, T, k = 100, 0.05, 0.3, 500, 1, 100
 
-    fcall = black_scholes(x_0, u, s, k)
+    fcall = black_scholes(x_0, u, s, k, T)
     print(fcall)
 
+#    m, w = 1000, 100
 #    fcall_hat, fcalls = montecarlo(x_0, u, s, n, T, m, w, k)
 #    print(fcall_hat)
 
-#    fcall_hat = binomial_tree(x_0, u, s, n)
+#    fcall_hat = binomial_tree(x_0, u, s, k, 5000)
 #    print(fcall_hat)
 
-    start = time.time()
-    fcall_hat = finite_differences(s, u, k, T, 1500)
-    end = time.time()
-    print(fcall_hat, end-start)
+#    fcall_hat = finite_differences(s, u, k, T, 500)
+#    print(fcall_hat)
