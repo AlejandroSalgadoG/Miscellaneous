3928b739ac5bfd8f4f0e3188751e1596a4350f57
Alejandro Salgado G <asalgad2@eafit.edu.co>
2020-12-03 10:20:46 -0500

    Adding more graphics to fourier coeficients

--- Fourier/Coefitients.py
+++ Fourier/Coefitients.py
@@ -4,20 +4,48 @@ import matplotlib.pyplot as plt
 
 n = 100
 dt = 1/n # inter sample time
-fs = 1/dt # sample frequency
 p = n*dt # period
-df = fs/n # frequency resolution
+df = 1/p # frequency resolution
 print(df)
 
-f = 6 # frequency
-a = 3 # amplitud
+f, f2 = 6, 32 # frequency
+a, a2 = 3, 3 # amplitude
+phi, phi2 = 0, 0 # phase
 
 t = np.arange(0,p,dt) # time
-x = a * np.cos( 2*np.pi*f*t )
+x = a * np.cos( 2*np.pi*f*t + phi) + a2 * np.cos( 2*np.pi*f2*t + phi2 ) # sample function
 
 nyquist = math.floor( t.size/2 ) + 1
 coef = np.fft.fft( x )[:nyquist] / n
 
-plt.scatter( np.arange(coef.size), abs(coef)*2, s=7 )
-plt.scatter( np.arange(coef.size), np.angle(coef), s=7 )
+fig, (ax1, ax2) = plt.subplots(2)
+
+ax1.scatter( np.arange(coef.size), abs(coef)*2, s=7 )
+ax2.scatter( np.arange(coef.size), np.angle(coef), s=7 )
+
+ax1.set_ylabel("Amplitude")
+ax2.set_ylabel("Phase")
+ax2.set_xlabel("Frequency")
+plt.show()
+
+waves = []
+for idx, c in enumerate(coef):
+    f, a, phi = idx, abs(c)*2, np.angle(c)
+    x_p = a * np.cos( 2*np.pi*f*t + phi )
+    waves.append( x_p )
+    
+waves = np.array( waves )
+waves[0] /= 2
+fourier_wave = np.sum( waves, axis=0 )
+
+plt.plot( t, waves[6] + waves[0] )
+plt.plot( t, x )
+plt.show()
+
+plt.plot( t, waves[32] + waves[0] )
+plt.plot( t, x )
+plt.show()
+
+plt.plot( t, fourier_wave )
+plt.plot( t, x )
 plt.show()
