094cb8e13a284a75ab4f2b05d0b2e13ab48befcb
Alejandro Salgado G <asalgad2@eafit.edu.co>
2021-03-31 12:34:16 -0500

    Improving Minimum Volume Ellipsoid method

--- MultStadistic/OutlierDetect/MinimumVolumeEllipsoid.py
+++ MultStadistic/OutlierDetect/MinimumVolumeEllipsoid.py
@@ -9,30 +9,40 @@ from Auxiliar import random_subsets
 data = pd.read_csv( "pulpfiber_simp.csv" )
 n,p = data.shape
 
-m = 4000 # number of combinations to be used
+m = 3000 # number of combinations to be used
 
 h = floor( (n+p+1)/ 2 )
 c = np.sqrt( chi2.ppf(h/n, p) )
 
-obj_fun = []
-
-subsets = random_subsets( np.arange(n), m, p+1 )
-for subset in subsets:
-    sample = data.iloc[subset,:]
+def get_estimations( sample ):
     u_bar = sample.mean()
-    s = sample.cov(ddof=0)
+    s = sample.cov()
     d_s = np.linalg.det(s)
     s_1 = np.linalg.inv( s )
+    return u_bar, s, s_1, d_s
+
+obj_fun = []
+
+print( "calculating subsets...", end="" )
+subsets = random_subsets( np.arange(n), m, p+1 )
+print( "done" )
+
+for i, subset in enumerate(subsets):
+    print( "%.2f%%" % (i/m*100), end="\r" ) # print progress
+    sample = data.iloc[subset,:]
+    u_bar, s, s_1, d_s = get_estimations( sample )
 
     dist = np.sort([ mahalanobis( x, u_bar, s_1 ) for _,x in data.iterrows() ])
-    obj = (dist[h] / c)**p * np.sqrt(d_s)
+
+    obj = (dist[h]/c)**p * np.sqrt(d_s)
     obj_fun.append( obj )
 
 best_subset = subsets[ np.argmin( obj_fun ) ]
-best_data = data.iloc[best_subset,:]
+best_sample = data.iloc[best_subset,:]
 
-best_u = best_data.mean()
-best_s = best_data.cov()
+best_u, best_s, best_s_1, d_s = get_estimations( best_sample )
+#best_dist = np.sort([ mahalanobis( x, best_u, best_s_1 ) for _,x in data.iterrows() ])
+#best_s *= best_dist[h]/c**2
 
-print( best_u )
-print( best_s )
+print( best_u.values )
+print( best_s.values )
